7c4b04605e2d89d2e70fddd3499f4e529eec78e1
Eduardo Veloso <107579577+EduardoVeloso06@users.noreply.github.com>
2024-08-08 17:02:31 -0300

    08/09

--- main.js
+++ main.js
@@ -59,4 +59,15 @@ function geraSenha(){
     senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
+    classificaSenha();
+}
+
+function classificaSenha() {
+    if(tamanhoSenha>11){
+        forcaSenha.classList.add('forte');
+    } else if (tamanhoSenha > 5 && tamanhoSenha<12){
+        forcaSenha.classList.add('media');
+    } else if (tamanhoSenha <=5){
+        forcaSenha.classList.add('fraca');
     }
+}
